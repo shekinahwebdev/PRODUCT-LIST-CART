aaf36348bf8f323dda0a40473e17725dae5eb864
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-04 22:26:22 +0000

    Remove unused useEffect import from FoodContainer component

--- src/components/FoodContainer.tsx
+++ src/components/FoodContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import data from "../data/data.json";
 import { EmptyPage } from "./EmptyPage";
 import Food from "./Food";
